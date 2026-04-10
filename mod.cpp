c3b50c0cbba262c2205e7f286b65557e466c81f6
Milo <milo@olympus-entertainment.com>
2026-04-10 17:42:51 -0400

    Add addon thumbnail and description (#16)

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+author = "Milo";
+name = "SQF Lab";
+actionName = "Updates";
+action = "";
+picture = "sqflab.paa";
+logoSmall = "sqflab.paa";
+logo = "sqflab.paa";
+overview = "A development toolkit which makes mission and addon building easier.";
